d4d3ecc3dd03496f811b0064f6efa2523a920835
Grbisba <polickarpow.dany@yandex.ru>
2024-09-11 19:50:44 +0300

    pgxpool writing was successfuly ended

--- fx/pgxpool.go
+++ fx/pgxpool.go
@@ -1,1 +1,77 @@
-package pgxpool
+package fx
+
+import (
+	"context"
+	"fmt"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	pgxUUID "github.com/vgarvardt/pgx-google-uuid/v5"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+	"time"
+)
+
+const (
+	RetryAttempts = 5
+	RetryDelay    = 3 * time.Second
+)
+
+// New opens new postgres connection, configures it and return prepared pool.
+func New(lc fx.Lifecycle, dbUri string, log *zap.Logger) (*pgxpool.Pool, error) {
+	var pool *pgxpool.Pool
+
+	configuredPool, err := pgxpool.ParseConfig(dbUri)
+	if err != nil {
+		return nil, fmt.Errorf("error while parsing db uri: %w", err)
+	}
+
+	configuredPool.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
+		pgxUUID.Register(conn.TypeMap())
+		return nil
+	}
+
+	pool, err = pgxpool.NewWithConfig(context.Background(), configuredPool)
+	if err != nil {
+		return nil, fmt.Errorf("postgres: init pgxpool: %w", err)
+	}
+
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			return TryWithAttemptsCtx(ctx, pool.Ping, RetryAttempts, RetryDelay)
+		},
+		OnStop: func(ctx context.Context) error {
+			pool.Close()
+			return nil
+		},
+	})
+
+	log.Info("created postgres client")
+
+	return pool, nil
+}
+
+// TryWithAttempts tries to get non-error result of calling function f with delay.
+func TryWithAttempts(f func() error, attempts uint, delay time.Duration) (err error) {
+	err = f()
+
+	if err == nil {
+		return nil
+	}
+
+	for i := uint(1); i < attempts; i++ {
+		if err = f(); err == nil {
+			return nil
+		}
+		zap.L().Warn("got error in attempter", zap.Uint("attempts", i+1), zap.NamedError("error", err))
+		time.Sleep(delay)
+	}
+	return err
+}
+
+// TryWithAttemptsCtx is helper function that calls TryWithAttempts with function f transformed to closure that does not
+// require ctx as necessary argument.
+func TryWithAttemptsCtx(ctx context.Context, f func(context.Context) error, attempts uint, delay time.Duration) (err error) {
+	return TryWithAttempts(func() error {
+		return f(ctx)
+	}, attempts, delay)
+}
---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/Package
+
+go 1.23.0
+
+require (
+	github.com/jackc/pgx-zap v0.0.0-20221202020421-94b1cb2f889f
+	github.com/jackc/pgx/v5 v5.7.1
+	github.com/vgarvardt/pgx-google-uuid/v5 v5.6.0
+	go.uber.org/fx v1.22.2
+	go.uber.org/zap v1.26.0
+)
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	go.uber.org/dig v1.18.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+)
